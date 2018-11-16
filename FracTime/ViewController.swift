627afd06a88644d2c4ecccadfdcfa26d8d437f92
Thomas Etcheverria <tetcheve@gmail.com>
2018-11-16 15:30:06 +0100

    Initial Commit

---
+++ FracTime/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FracTime
+//
+//  Created by Thomas Etcheverria on 16/11/2018.
+//  Copyright © 2018 Belette Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
